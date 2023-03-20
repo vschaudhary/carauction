8046e0037a286ddb0092fca94d34ccbccb3ba7d6
Vishal Singh <sl_vishal@os4w.de>
2023-03-20 17:22:41 +0530

    create login and register

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\API\RegisterController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('login', [RegisterController::class, 'login']);
+Route::get('Unauthorised', [RegisterController::class, 'Unauthorised'])->name('login');
+Route::post('register', [RegisterController::class, 'register']);
+     
+Route::middleware('auth:api')->group( function () {
+    Route::post('abc', [RegisterController::class, 'abc']);
 });
