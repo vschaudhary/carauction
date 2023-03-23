f56a006fdf06d0c1cffff0d7a97f057c8678bf4d
Vishal Singh <sl_vishal@os4w.de>
2023-03-23 11:12:59 +0530

    remove reset password route

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,8 +39,7 @@ class User extends Authenticatable
      */
     protected $hidden = [
         'password',
-        'remember_token',
-        'role_id'
+        'remember_token'
     ];
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\API\RegisterController;
-use App\Http\Controllers\Auth\PasswordResetController;
 
 /*
 |--------------------------------------------------------------------------
@@ -25,11 +24,6 @@ use App\Http\Controllers\Auth\PasswordResetController;
 Route::post('login', [RegisterController::class, 'login']);
 Route::get('Unauthorised', [RegisterController::class, 'Unauthorised'])->name('login');
 Route::post('register', [RegisterController::class, 'register']);
-
-//User Password Reset
-Route::post('password/create', [PasswordResetController::class, 'create']);
-Route::get('password/find/{token}', [PasswordResetController::class, 'find']);
-Route::post('password/reset', [PasswordResetController::class, 'reset']);
      
 Route::middleware('auth:api')->group( function () {
     Route::post('abc', [RegisterController::class, 'abc']);
