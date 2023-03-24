0be9cd5235d9c4dc7780cf97f3a12aa695816f31
Vishal Singh <sl_vishal@os4w.de>
2023-03-24 14:45:36 +0530

    Password reset remove

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,11 +14,9 @@ return new class extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->id();
             $table->string('email');
             $table->string('token');
             $table->timestamp('created_at')->nullable();
-            $table->timestamp('updated_at')->nullable();
         });
     }
 
